7ee1c470bd5d7678152994116902e70cb08c57ab
Eric Telkkälä <52538447+erictelkkala@users.noreply.github.com>
2024-12-02 18:38:59 +0200

    Format using prettier

--- src/__tests__/add.test.js
+++ src/__tests__/add.test.js
@@ -22,63 +22,62 @@ describe('add', () => {
             test('adds 1.1 + 2.2 to equal 3.3', () => {
                 expect(add(1.1, 2.2)).toBeCloseTo(3.3);
             });
-        })
+        });
 
         describe('string values', () => {
             test('adds "1" + "2" to equal "12"', () => {
-                expect(add("1", "2")).toEqual("12");
+                expect(add('1', '2')).toEqual('12');
             });
 
             test('adds "1.1" + "2.2" to equal "1.12.2"', () => {
-                expect(add("1.1", "2.2")).toEqual("1.12.2");
+                expect(add('1.1', '2.2')).toEqual('1.12.2');
             });
 
             test('adds "1.1" + 2.2 to equal "1.12.2"', () => {
-                expect(add("1.1", 2.2)).toEqual("1.12.2");
+                expect(add('1.1', 2.2)).toEqual('1.12.2');
             });
 
             test('adds 1.1 + "2.2" to equal "1.12.2"', () => {
-                expect(add(1.1, "2.2")).toEqual("1.12.2");
+                expect(add(1.1, '2.2')).toEqual('1.12.2');
             });
-        })
-    })
+        });
+    });
 
     describe('negative tests', () => {
         test('adds 1 + 5 to NOT equal 5', () => {
-            expect(add(1, 5)).not.toEqual(5)
-        })
+            expect(add(1, 5)).not.toEqual(5);
+        });
 
         test('adds undefined + undefined to equal 0', () => {
             expect(add(undefined, undefined)).toEqual(0);
-        })
+        });
 
         test('adds null + null to equal 0', () => {
             expect(add(null, null)).toEqual(0);
-        })
+        });
 
         test('adds null + 1 to equal 1', () => {
             expect(add(null, 1)).toEqual(1);
-        })
+        });
 
         test('adds 2 + null to equal 2', () => {
             expect(add(2, null)).toEqual(2);
-        })
+        });
 
         test('adds 1 + undefined to equal 1', () => {
             expect(add(1, undefined)).toEqual(1);
-        })
+        });
 
         test('adds undefined + 2 to equal 2', () => {
             expect(add(undefined, 2)).toEqual(2);
-        })
+        });
 
         test('adds "1" + undefined to equal "1"', () => {
-            expect(add("1", undefined)).toEqual("1");
-        })
+            expect(add('1', undefined)).toEqual('1');
+        });
 
         test('adds undefined + "2" to equal "2"', () => {
-            expect(add(undefined, "2")).toEqual("2");
-        })
-    })
-})
-
+            expect(add(undefined, '2')).toEqual('2');
+        });
+    });
+});
--- src/__tests__/capitalize.test.js
+++ src/__tests__/capitalize.test.js
@@ -3,49 +3,47 @@ import capitalize from '../capitalize';
 describe('capitalize', () => {
     describe('positive tests', () => {
         test('capitalizes string primitive "hello world!" to "Hello world!"', () => {
-            expect(capitalize("hello world!")).toBe("Hello world!");
+            expect(capitalize('hello world!')).toBe('Hello world!');
         });
 
         test('capitalizes string primitive "heLLO woRlD!" to "Hello world!"', () => {
-            expect(capitalize("heLLO woRlD!")).toBe("Hello world!");
+            expect(capitalize('heLLO woRlD!')).toBe('Hello world!');
         });
 
         test('capitalizes string primitive "HELLO WORLD!" to "Hello world!"', () => {
-            expect(capitalize("HELLO WORLD!")).toBe("Hello world!");
+            expect(capitalize('HELLO WORLD!')).toBe('Hello world!');
         });
 
         test('capitalizes string primitive "1234" to "1234"', () => {
-            expect(capitalize("1234")).toBe("1234");
+            expect(capitalize('1234')).toBe('1234');
         });
 
         test('capitalizes string object("hello world!") to "Hello world!"', () => {
-            expect(capitalize(new String("hello world!"))).toBe("Hello world!");
+            expect(capitalize(new String('hello world!'))).toBe('Hello world!');
         });
-    })
+    });
 
     describe('negative tests', () => {
         test('capitalizes number 1234 to "1234"', () => {
-            expect(capitalize(1234)).toBe("1234");
+            expect(capitalize(1234)).toBe('1234');
         });
 
         test('capitalizes float 1.2 to "1.2"', () => {
-            expect(capitalize(1.2)).toBe("1.2");
+            expect(capitalize(1.2)).toBe('1.2');
         });
 
         test('capitalizes string primitive "" to Undefined', () => {
-            expect(capitalize(""))
-            .toBe("")
-            .toHaveLength(0);
+            expect(capitalize('')).toBe('').toHaveLength(0);
         });
 
         // SKIPPED DUE TO BUGS
         test.skip('capitalizes null to ""', () => {
-            expect(capitalize(null)).toBe("").toHaveLength(0);
+            expect(capitalize(null)).toBe('').toHaveLength(0);
         });
 
         test.skip('capitalizes null to ""', () => {
-            expect(capitalize(undefined)).toBe("").toHaveLength(0);
+            expect(capitalize(undefined)).toBe('').toHaveLength(0);
         });
         // SKIPPED TESTS END
-    })
-})
+    });
+});
